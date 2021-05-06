fe1b9ceb021f279eb18ec8f6f105b5a30f2824d8
Sameeha Shaik <sameeha1100@gmail.com>
2021-05-06 11:20:19 +0530

    color slider task edited

---
+++ frontend/js/color.js
@@ -0,0 +1,19 @@
+function onColorRangeChange()
+{
+            // getElement p
+            let pE = document.getElementById("pColor");
+            let rE = document.getElementById("redSlider");
+            let gE = document.getElementById("greenSlider");
+            let bE = document.getElementById("blueSlider");
+            // getElement redSlider
+            // getElement greenSlider
+            // getElement blueSlider
+            let rgbprop = "rgb("+rE.value+","+gE.value+","+bE.value+")";
+            console.log(rgbprop);
+            // Get value of r, g, b from sliders
+            // Create a string rgb(r, g, b)
+            pE.innerHTML = rgbprop;
+            pE.style.backgroundColor = rgbprop;
+            // Set above string as innerHTML of
+            // Set rgb value as p.style.backgroundColor = 
+        }
