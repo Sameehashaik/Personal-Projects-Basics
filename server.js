7fea62c6c30e9e93ce9009e67b18819a23d03d5f
Sameeha Shaik <sameeha1100@gmail.com>
2021-05-19 02:14:33 +0530

    mongo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Table = require('./frontend/js/table');
+const Table = require('./frontend/js/Table');
 
 const app = express();
  
