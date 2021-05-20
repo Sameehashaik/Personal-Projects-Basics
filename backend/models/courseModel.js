cdd7570422409938fd470866e8df6ecd2889009e
Sameeha Shaik <sameeha1100@gmail.com>
2021-05-20 23:20:34 +0530

    housie

--- backend/models/courseModel.js
+++ backend/models/courseModel.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 var courseSchema = new mongoose.Schema({
     name:{type: String, required: true},
-    isdelete:{type:Boolean. requied: true}
+    isdelete:{type: Boolean. required: true}
 }
 module.exports = mongoose.model('Task', courseSchema)
--- server.js
+++ server.js
@@ -8,48 +8,44 @@ app.use(express.static(__dirname+"/frontend"));
 
 dbConnectLib.connect();
 
-// var password = process.env.Mongo_atlas_PASSWORD;
-// var connectionString = "mongodb+srv://sameeha_shaik:alohamora@cluster0.tdhog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-// mongoose.connect(connectionString, {useUnifiedTopology: true, useNewUrlParser: true});
-// mongoose.connection.on('connected', function(){
-//     console.log("Database Connected");
-// });
-
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.sendFile('frontend/html/index.html',{root:__dirname})
 })
 
-app.get("/Resume", function(req, res){
+app.get("/Resume", function(req, res) {
     res.sendFile('frontend/html/resume.html',{root:__dirname})
 })
 
-app.get("/Google", function(req, res){
+app.get("/Google", function(req, res) {
     res.sendFile('frontend/html/google.html',{root:__dirname})
 })
 
-app.get("/Color", function(req, res){
+app.get("/Color", function(req, res) {
     res.sendFile('frontend/html/color.html',{root:__dirname})
 })
 
-app.get("/Login", function(req, res){
+app.get("/Login", function(req, res) {
     res.sendFile('frontend/html/login.html',{root:__dirname})
 })
 
-app.get("/Register", function(req, res){
+app.get("/Register", function(req, res) {
     res.sendFile('frontend/html/register.html',{root:__dirname})
 })
 
-app.get("/Todo", function(req, res){
+app.get("/Todo", function(req, res) {
     res.sendFile('frontend/html/todo.html',{root:__dirname})
 })
 
-app.get("/TodoCRUD", function(req, res){
+app.get("/TodoCRUD", function(req, res) {
     res.sendFile('frontend/html/todocrud.html',{root:__dirname})
 })
 
-app.get("/CRUD", function(req, res){
+app.get("/CRUD", function(req, res) {
     res.sendFile('frontend/html/crud.html',{root:__dirname})
+})
+
+app.get("/Housie", function(req, res) {
+    res.sendFile('frontend/html/housie.html',{root:__dirname})
 })
 
 // Heroku will automatically set an environment variable called PORT
@@ -64,24 +60,24 @@ app.listen(PORT, function(){
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-var a={
+var a = {
     "task":[]
 };
 
-app.get('/api/todo', function(req, res){
+app.get('/api/todo', function(req, res) {
     res.json(a);
 })
 
-app.post('/api/todo', function(req, res){
+app.post('/api/todo', function(req, res) {
     var newUser= req.body;
     a.task.push(newUser.task)
     res.json(a)
  })
 
- app.delete('/api/todo/:id', function(req, res){
+ app.delete('/api/todo/:id', function(req, res) {
     var i=req.params.id
-    if(i==-1){
-        for(var j=0;j<a.task.length;++j){
+    if(i==-1) {
+        for(var j=0;j<a.task.length;++j) {
             a.task.splice(j,1)
             console.log(a.task[j])
         }
@@ -89,25 +85,24 @@ app.post('/api/todo', function(req, res){
     a.task.splice(i,1)
 })
 
-app.put('/api/todo/:id', function(req, res){
+app.put('/api/todo/:id', function(req, res) {
     var i=req.params.id
     a.task[i]="<s>"+a.task[i]+"</s>" 
 })
 
 //app.js
 
-app.get('/crud/get',async function(req, res){
-    //res.json(a);
+app.get('/crud/get',async function(req, res) {
     await Table.find()
-    .then((result)=>{
-     res.send(result)
+    .then((result) => {
+        res.send(result)
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err)
     })
 })
 
-app.post('/crud/post',function(req, res){
+app.post('/crud/post',function(req, res) {
     var newt= req.body;
     console.log(newt)
     const table= new Table({
@@ -119,26 +114,25 @@ app.post('/crud/post',function(req, res){
     //res.json(a)
  })
 
- app.delete('/crud/del:id', function(req, res){
+ app.delete('/crud/del:id', function(req, res) {
     var i=req.params.id
     console.log(i)
-        Table.findByIdAndDelete(i,function(err,orb){
-        if(err)
-        console.log("ERROR:"+err)
-        else 
-        console.log("SUCCESS")
+        Table.findByIdAndDelete(i,function(err,orb) {
+            if(err)
+            console.log("ERROR:"+err)
+            else 
+            console.log("SUCCESS")
     })
 })
 
-app.put('/crud/put:id', function(req, res){
+app.put('/crud/put:id', function(req, res) {
     var i=req.params.id
-    Table.findById(i,function(err,obj){
+    Table.findById(i,function(err,obj) {
         if(err)
         console.log("ERROR:"+err)
         else {
             console.log(obj.Articels)
-        var obj={Articels: obj.Articels }
-        //Table.findByIdAndUpdate(i,obj,function(){})
+            var obj={Articels: obj.Articels}
         }
     })
 })
